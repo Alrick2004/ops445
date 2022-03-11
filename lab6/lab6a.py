a4414531f679e5494b4ca0fbccdb2ea8daf44d0f
alrick2004 <ahscott@myseneca.ca>
2022-03-10 21:52:10 -0500

    add grade point average for courses

--- lab6/lab6a.py
+++ lab6/lab6a.py
@@ -16,3 +16,14 @@ class Student:
     # Add a new course and grade to students record
     def addGrade(self, course, grade):
         self.courses[course] = grade
+
+ # Calculate the grade point average of all courses and return a string
+    def displayGPA(self):
+        gpa = 0.0
+        for course in self.courses.keys():
+            gpa = gpa + self.courses[course]
+        try:    
+            gpa = gpa / len(self.courses)
+        except ZeroDivisionError:
+            pass
+        return 'GPA of student ' + self.name + ' is ' + str(gpa)
