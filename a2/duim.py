a2755d088d03de5fde57855ef2485f850e3e5ab2
alrick2004 <ahscott@myseneca.ca>
2022-04-05 17:58:38 -0400

    Assign2 Final Completion

--- a2/duim.py
+++ a2/duim.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python                                           
-# Author ID: ahscott
+#!/usr/bin/env python3
+# Student ID: ahscott
 
 import subprocess, sys
 import os
@@ -27,11 +27,16 @@ Date:
 def parse_command_args():
     "Set up argparse here. Call this function inside main."
     parser = argparse.ArgumentParser(description="DU Improved -- See Disk Usage Report with bar charts",epilog="Copyright 2022")
+    parser.add_argument("-H","--human-readable",action="store_true",help="print sizes in human readable format (e.g. 1K 23M 2G)")
     parser.add_argument("-l", "--length", type=int, default=20, help="Specify the length of the graph. Default is 20.")
+    parser.add_argument('directory', metavar='target', type=ascii, nargs=1,
+                    help='The directory to scan.')
+    
     # add argument for "human-readable". USE -H, don't use -h! -h is reserved for --help which is created automatically.
     # check the docs for an argparse option to store this as a boolean.
     # add argument for "target". set number of args to 1.
     args = parser.parse_args()
+    return args
 
 
 def percent_to_graph(percent, total_chars):
@@ -60,10 +65,33 @@ def call_du_sub(location):
 def create_dir_dict(alist):
     "gets a list from call_du_sub, returns a dictionary which should have full"
     "directory name as key, and the number of bytes in the directory as the value."
-    pass
+    dic = {}
+    arr1 = alist[0].decode('utf-8').strip().split('\n')
+    for i in arr1:
+       arr2 = i.split('\t')
+       if len(arr2) > 1: 
+       	 dic[arr2[1]] = arr2[0]		
+    return(dic)
+    	
 
 
 if __name__ == "__main__":
-    pass
+    parse_command_args()
 
-
+    if len(sys.argv) > 1 :
+      print("You cannot enter more than 2 arguments")
+      sys.exit(-1)
+    elif len(sys.argv) < 2:
+      alist = call_du_sub('./')
+    else:
+      alist = call_du_sub(sys.argv[1])
+    new_dict = create_dir_dict(alist)
+    total_size = 0.0
+    for key,value in new_dict.items():
+        
+        total_size += float(value[0:len(value) - 1])
+    
+    for key,value in new_dict.items():
+        if key != sys.argv[1]:
+            value = float(value[0:len(value) - 1])
+            print(str((value/total_size)*100) +" "+ percent_to_graph(value/total_size,10) + " " + key)    
